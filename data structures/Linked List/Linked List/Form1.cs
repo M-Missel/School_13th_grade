fc72bd603621b5dafe89e15d0b7c53b4de37cdcd
Axtinator <m.missel@protonmail.com>
2024-07-04 21:45:46 +0200

    Homework

---
+++ data structures/Linked List/Linked List/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Linked_List.src;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linked_List
+{
+    public partial class Form1 : Form
+    {
+        private OwnList<int> list = new OwnList<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fillList()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddLast(i);
+            }
+        }
+    }
+}
